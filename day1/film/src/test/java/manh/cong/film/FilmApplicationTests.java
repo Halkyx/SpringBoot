4eb6e5a568c8326bc00a190d65663dac2287374a
Cong Manh <tcmanh2002@gmail.com>
2021-09-17 16:46:01 +0700

    day1 homework updated

---
+++ day1/film/src/test/java/manh/cong/film/FilmApplicationTests.java
@@ -0,0 +1,13 @@
+package manh.cong.film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
